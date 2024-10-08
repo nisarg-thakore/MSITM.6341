eff36db55f02049500f208ddb1bbcc78311ec8ea
sweety951517 <143243225+sweety951517@users.noreply.github.com>
2024-10-08 14:30:18 -0500

    my hangman exercise code

---
+++ hangman/hangman_module.py
@@ -0,0 +1,23 @@
+#  write code as a module accepting word and guessed letters
+import random
+# return the word
+
+# define the list of the words
+# get the random word from the given list as a test word
+# define maximum attempts
+listOfWords = ['github', 'agile', 'development', 'computing', 'information']
+MaxAttemptsAllowed = 6
+
+def get_word():
+    return random.choice(listOfWords)
+
+# if guessed letters are in the word, display the letter in that position of the word and remaining place mask as '_'
+def IdentifyMissingLetters(word, guessed_letters):
+    display = ""
+    for letter in word:
+        if letter in guessed_letters:
+            display+= letter
+        else:
+            display+= "_"
+    return(display)
+
