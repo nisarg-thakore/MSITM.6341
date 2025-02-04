8d7737fbe6bed9820d68a74549354d2501939283
nisarg-thakore <84564480+nisarg-thakore@users.noreply.github.com>
2025-02-03 20:53:23 -0600

    Update assignment.py

--- Lesson01/Assignments/assignment.py
+++ Lesson01/Assignments/assignment.py
@@ -6,8 +6,48 @@
 # 2. Perform the operation based on the operator.
 # 3. Print the result.
 
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+print("Please select operation -\n" \
+        "1. Add\n" \
+        "2. Subtract\n" \
+        "3. Multiply\n" \
+        "4. Divide\n")
+
+select = int(input("Select operations form 1, 2, 3, 4 :"))
+
+number_1 = int(input("Enter first number: "))
+number_2 = int(input("Enter second number: "))
+
+if select == 1:
+    print(number_1, "+", number_2, "=",
+                    add(number_1, number_2))
+
+elif select == 2:
+    print(number_1, "-", number_2, "=",
+                    subtract(number_1, number_2))
+
+elif select == 3:
+    print(number_1, "*", number_2, "=",
+                    multiply(number_1, number_2))
+
+elif select == 4:
+    print(number_1, "/", number_2, "=",
+                    divide(number_1, number_2))
+else:
+    print("Invalid input")
 
 
+  
 # 2. Area of a Rectangle
 # Write a program that takes the length and width of a rectangle as input and calculates the area of the rectangle.
 
@@ -15,3 +55,9 @@
 # 1. Take the length and width of the rectangle as input.
 # 2. Calculate the area of the rectangle.
 # 3. Print the area.
+
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+area = length * width
+print("The area of the rectangle is:", area)
+
