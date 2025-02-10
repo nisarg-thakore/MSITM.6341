83168d6df1266ad18b5060eb523224958e15c40d
dnsabby <dns608@gmail.com>
2025-02-10 16:33:18 -0600

    Adding L3 materials.

--- Lesson03/Assignments/assignment.py
+++ Lesson03/Assignments/assignment.py
@@ -4,5 +4,6 @@
 
 # Task:
 # Convert your rock-paper-scissors game into a class-based implementation.
+# Move the class into a separate module and import it into your main script.
 
 # TODO: Define the Logger class and implement file logging methods
